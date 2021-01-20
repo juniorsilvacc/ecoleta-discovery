28643761f68c19dfcaf6f97d9e4c9896f08c00cc
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-01-20 19:10:16 -0300

    nunjucks p1

--- src/server.js
+++ src/server.js
@@ -6,20 +6,29 @@ const server = express()
 server.use(express.static("public"))
 
 
+
+// utilizando template njk
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // Configurar caminhos na minha aplicação
 // Pagina inicial
 // req: requisição
 // res: resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
